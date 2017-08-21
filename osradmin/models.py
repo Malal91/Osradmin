12cf429c9cf0c4d38dfc06f2237f341f2791223a
Malal <controlmalal@gmail.com>
2017-08-21 08:40:10 +0000

    delete randomslug

--- osradmin/models.py
+++ osradmin/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from randomslugfield import RandomSlugField
 
 class Connexion(models.Model) :
     user = models.OneToOneField(User)
